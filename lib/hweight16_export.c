ad495b2a85a0976bec540e2310937a8f9d423f0e
Martin Alejandro Oviedo <quamagi@hotmail.com>
2026-07-30 22:35:16 -0300

    Fix hweight export wrappers: include linux/bitops.h for symbol declaration

--- lib/hweight16_export.c
+++ lib/hweight16_export.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
+#include <linux/bitops.h>
 #include <linux/export.h>
 
-extern unsigned int spasm_hweight16(unsigned int w);
 EXPORT_SYMBOL(__sw_hweight16);
